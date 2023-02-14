adf4f7433e3658ed8f10a1458a981bc9f78b5255
Rexth-ru <119601642+Rexth-ru@users.noreply.github.com>
2023-02-14 13:36:04 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -6,15 +6,22 @@ import static java.util.Map.Entry.comparingByValue;
 
 public class Main {
     public static void main(String[] args) {
-
+        
+// read the input from the console and remove punctuation and space
+        
         Scanner scanner = new Scanner(System.in);
         String str = scanner.nextLine();
         str = str.replaceAll("\\p{Punct}|\\p{Space}"," ");
-
+        
+// count the number of words
+        
         List<String> stringList = new ArrayList<>(List.of(str.split(" ")));
         long n = stringList.stream().filter((x) -> !x.equals("")).count();
         System.out.println("В тексте "+n+" слов");
-
+    
+//print the most frequently mentioned words (10), ordered by the number of mentions in
+//reverse order. In case of the same number of mentions, the words should be sorted alphabetically.
+        
         Map<String, Long> map = stringList.stream().filter((x) -> !x.equals(""))
                 .collect(Collectors.groupingBy(Function.identity(),Collectors.counting()));
         map.entrySet().stream().sorted(Entry.comparingByKey())
